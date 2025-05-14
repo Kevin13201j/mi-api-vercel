fd6c04bcfc60e3fd8d1657fa4a7247f9ed0c7121
Kevin Amaguaña <71292836+Kevin13201j@users.noreply.github.com>
2025-05-14 15:58:58 -0500

    Restructura completa para Vercel sin Docker

--- api.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('¡Hola desde Vercel con Docker!'));
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
