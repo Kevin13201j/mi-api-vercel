a562345a95ea4859bbcf84f8d3d998ae7755e1d9
Kevin Amaguaña <71292836+Kevin13201j@users.noreply.github.com>
2025-05-14 19:04:49 -0500

    Integracion con clerk

--- api/index.js
+++ api/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const { ClerkExpressWithAuth, requireAuth } = require('@clerk/clerk-sdk-node');
 
 const app = express();
 const router = express.Router();
 
+// Middleware Clerk
+app.use(ClerkExpressWithAuth());
+
+// Ruta pública
 router.get('/', (req, res) => {
-  res.send('¡Hola desde Vercel funciona perfectamente!');
+  res.send('Ruta pública: ¡Hola desde Vercel sin Docker y funcionando!');
+});
+
+// Ruta protegida
+router.get('/protegida', requireAuth(), (req, res) => {
+  res.send(`Ruta protegida: ¡Hola ${req.auth.userId}!`);
 });
 
 app.use('/', router);
 
-// exporta como función serverless
+// Exportar como Serverless
 module.exports = app;
 module.exports.handler = serverless(app);
